514f610c575c26716e1b851c79c090ee279693b0
cain <bys20411@gmail.com>
2024-02-09 22:21:37 +0900

    feat: make service transactional

--- src/main/java/gunb0s/toy/ciazza/user/service/EducatorService.java
+++ src/main/java/gunb0s/toy/ciazza/user/service/EducatorService.java
@@ -5,12 +5,15 @@ import gunb0s.toy.ciazza.user.entity.Educator;
 import gunb0s.toy.ciazza.user.repository.EducatorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class EducatorService {
 	private final EducatorRepository educatorRepository;
 
+	@Transactional
 	public Long create(CreateEducatorDto createEducatorDto) {
 		Educator educator = Educator.builder()
 				.name(createEducatorDto.getName())
--- src/main/java/gunb0s/toy/ciazza/user/service/StudentService.java
+++ src/main/java/gunb0s/toy/ciazza/user/service/StudentService.java
@@ -5,12 +5,15 @@ import gunb0s.toy.ciazza.user.entity.Student;
 import gunb0s.toy.ciazza.user.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class StudentService {
 	private final StudentRepository studentRepository;
 
+	@Transactional
 	public Long create(CreateStudentDto createStudentDto) {
 		Student student = Student.builder()
 				.name(createStudentDto.getName())
