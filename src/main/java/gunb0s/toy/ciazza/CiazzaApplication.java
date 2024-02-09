c350c2fafc6f32a9ebdc899adf9853fa15ddadbf
cain <bys20411@gmail.com>
2024-02-09 18:16:44 +0900

    feature: initial setting

---
+++ src/main/java/gunb0s/toy/ciazza/CiazzaApplication.java
@@ -0,0 +1,13 @@
+package gunb0s.toy.ciazza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiazzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiazzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gunb0s/toy/ciazza/CiazzaApplicationTests.java
@@ -0,0 +1,13 @@
+package gunb0s.toy.ciazza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiazzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
