6abb5cecd6a691f3d0048df1f4e9876f0a25642d
cain <bys20411@gmail.com>
2024-02-09 22:21:10 +0900

    feat: auto insert create, update date column

--- src/main/java/gunb0s/toy/ciazza/CiazzaApplication.java
+++ src/main/java/gunb0s/toy/ciazza/CiazzaApplication.java
@@ -2,7 +2,9 @@ package gunb0s.toy.ciazza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CiazzaApplication {
 
