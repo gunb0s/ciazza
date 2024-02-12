203c170d2036956a875573795c81788502efae50
cain <gunbos1031@gmail.com>
2024-02-12 17:00:47 +0900

    fix: remove comments of comment dto

--- src/main/java/gunb0s/toy/ciazza/comment/controller/dto/CommentDto.java
+++ src/main/java/gunb0s/toy/ciazza/comment/controller/dto/CommentDto.java
@@ -3,9 +3,6 @@ package gunb0s.toy.ciazza.comment.controller.dto;
 import gunb0s.toy.ciazza.comment.entity.Comment;
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Getter
 public class CommentDto {
 	private Long id;
@@ -13,7 +10,6 @@ public class CommentDto {
 	private Long parentCommentId;
 	private Long userId;
 	private String username;
-	private List<CommentDto> childComments = new ArrayList<>();
 
 	public CommentDto(Comment comment) {
 		this.id = comment.getId();
